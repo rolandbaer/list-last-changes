433d45962570faec2523562b5b5c0fbd78dc836e
rbaer <rbaer@gmx.ch>
2013-12-14 16:45:00 +0100

    Version 0.3.0

--- list-last-changes.php
+++ list-last-changes.php
@@ -1,9 +1,9 @@
-﻿<?php
+<?php
 /**
  * Plugin Name: List Last Changes
- * Plugin URI: http://www.rolandbaer.ch
+ * Plugin URI: http://www.rolandbaer.ch/
  * Description: Shows a list of the last changes of a wordpress installation.
- * Version: 0.2.0
+ * Version: 0.3.0
  * Author: Roland Bär
  * Author URI: http://www.rolandbaer.ch
  * License: GPLv2
@@ -24,54 +24,125 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
- 
-/**
- * Output of widget
- */
-function list_last_changes_widget_init_sidebarwidget() {
-	echo '<aside id="list_last_changes_widget_1" class="widget list_last_changes_widget">' . "\n";
-	echo ' <h3 class="widget-title">Letzte &Auml;nderungen</h3>' . "\n";
-	echo " <ul>\n";
-	
-	$mypages = wp_get_pages(array('sort_column' => 'post_modified', 'sort_order' => 'asc', 'show_date' => 'modified'/*, 'number' => '5'*/ ));
-	usort($mypages, 'sort_pages_by_date_desc');
-	$count = min(count($mypages), 5);
-	for($i = 0; $i < $count; $i++) { 
-		$post = $mypages[$i];
-		setup_postdata($post);	
-		echo '  <li class="list_last_changes_title">'. "\n" . '   <a href="' . get_page_link( $post->ID ) .'">' . $post->post_title . "</a>\n";
-		echo '   <span class="list_last_changes_date">' . date_i18n(get_option('date_format') ,strtotime($post->post_modified)) . "</span>\n  </li>\n";
+
+class ListLastChangesWidget extends WP_Widget {
+
+	function ListLastChangesWidget() {
+	    $widget_ops = array('classname' => 'widget_list_last_changes', 'description' => __('Shows a list of the last changes of a wordpress installation') );
+         
+        $this->WP_Widget('list-last-changes-widget', __('List Last Changes'), $widget_ops);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	function widget( $args, $instance ) {
+		extract($args);
+	 
+		//  Get the title of the widget and the specified number of elements
+		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+		$number = empty($instance['number']) ? ' ' : $instance['number'];
+
+		echo '<aside id="list_last_changes_widget_1" class="widget list_last_changes_widget">' . "\n";
+		echo ' <h3 class="widget-title">' . $title . '</h3>' . "\n";
+		echo " <ul>\n";
+		
+		$mypages = $this->wp_get_pages(array('sort_column' => 'post_modified', 'sort_order' => 'asc', 'show_date' => 'modified'));
+		usort($mypages, 'sort_pages_by_date_desc');
+		$count = min(count($mypages), $number);
+		for($i = 0; $i < $count; $i++) { 
+			$post = $mypages[$i];
+			setup_postdata($post);	
+			echo '  <li class="list_last_changes_title">'. "\n" . '   <a href="' . get_page_link( $post->ID ) .'">' . $post->post_title . "</a>\n";
+			echo '   <span class="list_last_changes_date">' . date_i18n(get_option('date_format') ,strtotime($post->post_modified)) . "</span>\n  </li>\n";
+		}
+		
+		echo " </ul>\n";
+		echo "</aside>\n";	
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = strip_tags($new_instance['number']);
+	 
+		return $instance;
+	}
+
+	function form( $instance ) {
+		//  Assigns values
+		$instance = wp_parse_args( (array) $instance, array( 'title' => 'Last Changes', 'number' => '5' ) );
+		$title = strip_tags($instance['title']);
+		$number = strip_tags($instance['number']);
+		?>
+			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php echo __('Title'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+			<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php echo __('Number'); ?>: <input class="widefat" id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo attribute_escape($number); ?>" /></label></p>
+		<?php
 	}
 	
-	echo " </ul>\n";
-	echo "</aside>\n";	
+	/**
+	 * Get Pages
+	 */
+	function wp_get_pages($args = '') {
+		if ( is_array($args) )
+			$r =  &$args;
+		else
+			parse_str($args, $r);
+		
+		$defaults = array('depth' => 0, 'show_date' => '', 'date_format' => get_option('date_format'),
+			'child_of' => 0, 'exclude' => "", 'title_li' => __('Pages'), 'echo' => 1, 'authors' => '', 'sort_column' => 'menu_order, post_title');
+		$r = array_merge($defaults, $r);
+		
+		$output = '';
+		$current_page = 0;
+		
+		// sanitize, mostly to keep spaces out
+		$r['exclude'] = preg_replace('[^0-9,]', '', $r['exclude']);
+		
+		// Allow plugins to filter an array of excluded pages
+		$r['exclude'] = implode(',', apply_filters('wp_list_pages_excludes', explode(',', $r['exclude'])));
+		
+		// Query pages.
+		$pages = get_pages($r);
+		
+		return $pages;
+	}
 }
 
-/**
- * Sort by modified date ascending
+function list_last_changes_register_widgets() {
+	register_widget( 'ListLastChangesWidget' );
+}
+
+add_action( 'widgets_init', 'list_last_changes_register_widgets' );
+
+	
+	/**
+	 * Sort by modified date ascending
+	 */
+	function sort_pages_by_date($a, $b){
+		if ($a->post_modified == $b->post_modified)
+			return 0;
+		if ($a->post_modified < $b->post_modified)
+			return -1;
+		return 1;
+	}
+
+	/**
+	 * Sort by modified date descending
+	 */
+	function sort_pages_by_date_desc($a, $b){
+		return sort_pages_by_date($a, $b) * -1;
+	}
+	
+	/**
+ * Register style sheet.
  */
-function sort_pages_by_date($a, $b){
-    if ($a->post_modified == $b->post_modified)
-        return 0;
-    if ($a->post_modified < $b->post_modified)
-        return -1;
-    return 1;
-}
-
-/**
- * Sort by modified date descending
- */
-function sort_pages_by_date_desc($a, $b){
-    return sort_pages_by_date($a, $b) * -1;
-}
-
-/**
- * Init Widget
- */ 
-function list_last_changes_widget_init()
-{
-   wp_register_sidebar_widget('1',__('List Last Changes'), 'list_last_changes_widget_init_sidebarwidget');
-}
+add_action( 'wp_enqueue_scripts', 'list_last_changes_register_plugin_styles' );
 
 /**
  * Register style sheet.
@@ -80,45 +151,3 @@ function list_last_changes_register_plugin_styles() {
 	wp_register_style( 'list-last-changes', plugins_url( 'list-last-changes/css/list-last-changes.css' ) );
 	wp_enqueue_style( 'list-last-changes' );
 }
-
-/**
- * Get Pages
- */
-function wp_get_pages($args = '') {
-	if ( is_array($args) )
-		$r =  &$args;
-	else
-		parse_str($args, $r);
-	
-	$defaults = array('depth' => 0, 'show_date' => '', 'date_format' => get_option('date_format'),
-		'child_of' => 0, 'exclude' => "", 'title_li' => __('Pages'), 'echo' => 1, 'authors' => '', 'sort_column' => 'menu_order, post_title');
-	$r = array_merge($defaults, $r);
-	
-	$output = '';
-	$current_page = 0;
-	
-	// sanitize, mostly to keep spaces out
-	$r['exclude'] = preg_replace('[^0-9,]', '', $r['exclude']);
-	
-	// Allow plugins to filter an array of excluded pages
-	$r['exclude'] = implode(',', apply_filters('wp_list_pages_excludes', explode(',', $r['exclude'])));
-	
-	// Query pages.
-	$pages = get_pages($r);
-	
-	return $pages;
-}
-
-/**
- * Add Actions
- */
- 
-/**
- * Initialize Plugin
- */
-add_action("plugins_loaded", "list_last_changes_widget_init");
-
-/**
- * Register style sheet.
- */
-add_action( 'wp_enqueue_scripts', 'list_last_changes_register_plugin_styles' );
---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== List Last Changes ===Contributors: rolandbaerTags: last changes, pagesRequires at least: 3.0.0Tested up to: 3.8.0License: GPLv2 or laterStable tag: 0.3.0
+Shows a list of the last changes in the widget area.
+== Description ==Shows a list of the last changes in the widget area.
+Additional features include: * Select the number of entries in the list
+== Changelog ==
+= 0.3.0 =
+* Changed to class* Added configuration dialog for number of entries and title
+= 0.2.0 =
+* First usage on own website
+== Installation ==
+1. Upload the `list-last-changes` folder to your `/wp-content/plugins/` directory2. Activate the plugin using the `Plugins` menu in WordPress3. Add Last Changes Widget to your page4. Adjust the Last Changes Options as you prefer them. 
+== Frequently Asked Questions ==
+= Where are your Frequently Asked Questions? Why aren't they here? =
+Because no questions were asked.
+
+== Upgrade Notice ==
+* 0.3.0 First official release.
